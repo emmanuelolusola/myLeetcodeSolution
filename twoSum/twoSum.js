5ad482448e9083ac319ce2effd5ad3668d139317
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2022-07-24 23:27:45 +0100

    leetcode-01

---
+++ twoSum/twoSum.js
@@ -0,0 +1,17 @@
+function twoSum(nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let numOne = nums[i];
+    let numTwo = target - numOne;
+
+    if (map.has(numTwo)) return [i, map.get(numTwo)];
+    map.set(numOne, i);
+  }
+}
+
+// TESTCASES
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([], 10));
+console.log(twoSum([2, 4, 9], 10));
