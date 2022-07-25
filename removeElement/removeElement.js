db776e0473cae583e8a266a8c8f9f8dcd8209c7e
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2022-07-25 20:56:41 +0100

    leetcode-27

---
+++ removeElement/removeElement.js
@@ -0,0 +1,16 @@
+function removeElement(nums, val) {
+  if (!nums) return 0; // edge case
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    }
+  }
+  return nums.length;
+}
+
+// TESTCASES
+console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+console.log(removeElement([3, 2, 2, 3], 5));
+console.log(removeElement([], 5));
