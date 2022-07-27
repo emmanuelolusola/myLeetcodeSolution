66957c6e9219157dae0bef2a5370a78fe24336cc
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2022-07-27 19:52:10 +0100

    leetcode-58

--- lengthOfLastWord/lengthOfLastWord.js
+++ lengthOfLastWord/lengthOfLastWord.js
@@ -1,3 +1,9 @@
 function lengthOfLastWord(s) {
   return s.trim().split(" ").pop().length;
 }
+
+// TESTCASES
+console.log(lengthOfLastWord("This is Emmanuel Olusola"));
+console.log(lengthOfLastWord("Hello World"));
+console.log(lengthOfLastWord("  This is Mary Jane       "));
+console.log(lengthOfLastWord(""));
