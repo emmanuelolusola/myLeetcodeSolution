36ab91d6815a2f09bed254846674f5fc82abd923
emmanuelolusola <emmanuelolusolaoyeleke@gmail.com>
2022-07-30 23:53:20 +0100

    leetcode-88

---
+++ mergeSortedArray/mergeSortedArray.js
@@ -0,0 +1,13 @@
+function merge(nums1, m, nums2, n) {
+  // USING TWO-POINTER => O(N)
+  let p1 = m - 1,
+    p2 = n - 1,
+    i = m + n - 1;
+  while (p2 >= 0) {
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[i--] = nums1[p1--];
+    } else {
+      nums1[i--] = nums2[p2--];
+    }
+  }
+}
